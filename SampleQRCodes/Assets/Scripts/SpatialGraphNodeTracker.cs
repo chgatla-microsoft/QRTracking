0d1d022ec25305ac032484d4da33c6b443cde967
yl <yl@microsoft.com>
2021-02-03 23:58:27 -0800

    SpatialGraphNode lives iif mirage returns valid CoordinateSystem

--- SampleQRCodes/Assets/Scripts/SpatialGraphNodeTracker.cs
+++ SampleQRCodes/Assets/Scripts/SpatialGraphNodeTracker.cs
@@ -23,8 +23,11 @@ namespace QRTracking
 
             set
             {
-                _id = value;
-                node = SpatialGraphNode.FromStaticNodeId(_id);
+                if (_id != value)
+                {
+                    _id = value;
+                    node = SpatialGraphNode.FromStaticNodeId(_id);
+                }
             }
         }
 
@@ -40,7 +43,23 @@ namespace QRTracking
         // Update is called once per frame
         void Update()
         {
-            node.UpdateLocation(gameObject);
+            if (node == null)
+            {
+                node = SpatialGraphNode.FromStaticNodeId(_id);
+            }
+
+            if (node != null && node.TryLocate(out Pose pose))
+            {
+                // If there is a parent to the camera that means we are using teleport and we should not apply the teleport
+                // to these objects so apply the inverse
+                if (CameraCache.Main.transform.parent != null)
+                {
+                    pose = pose.GetTransformedBy(CameraCache.Main.transform.parent);
+                }
+
+                gameObject.transform.SetPositionAndRotation(pose.position, pose.rotation);
+                //Debug.Log("Id= " + id + " QRPose = " +  pose.position.ToString("F7") + " QRRot = "  +  pose.rotation.ToString("F7"));
+            }
         }
 
         private class SpatialGraphNode
@@ -52,86 +71,64 @@ namespace QRTracking
 
             public static SpatialGraphNode FromStaticNodeId(System.Guid id)
             {
-                return new SpatialGraphNode()
-                {
-                    Id = id,
 #if WINDOWS_UWP
-                    CoordinateSystem = Windows.Perception.Spatial.Preview.SpatialGraphInteropPreview.CreateCoordinateSystemForNode(id),
+                var coordinateSystem = Windows.Perception.Spatial.Preview.SpatialGraphInteropPreview.CreateCoordinateSystemForNode(id);
+                return coordinateSystem == null ? null :
+                    new SpatialGraphNode()
+                    {
+                        Id = id,
+                        CoordinateSystem = coordinateSystem
+                    };
 #endif
-                };
+                return null;
             }
 
 
-            public void UpdateLocation(GameObject gameObject)
+            public bool TryLocate(out Pose pose)
             {
-                {
+                pose = Pose.identity;
+
 #if WINDOWS_UWP
-                    if (CoordinateSystem == null)
-                    {
-                        CoordinateSystem = Windows.Perception.Spatial.Preview.SpatialGraphInteropPreview.CreateCoordinateSystemForNode(Id);
-
-                        if (CoordinateSystem == null)
-                        {
-                            Debug.Log("Id= " + Id + " Failed to acquire coordinate system");
-                        }
-                    }
-
-                    if (CoordinateSystem != null)
-                    {
-                        Quaternion rotation = Quaternion.identity;
-                        Vector3 translation = new Vector3(0.0f, 0.0f, 0.0f);
+                Quaternion rotation = Quaternion.identity;
+                Vector3 translation = new Vector3(0.0f, 0.0f, 0.0f);
                     
-                        System.IntPtr rootCoordnateSystemPtr = UnityEngine.XR.WindowsMR.WindowsMREnvironment.OriginSpatialCoordinateSystem;
-                        SpatialCoordinateSystem rootSpatialCoordinateSystem = (SpatialCoordinateSystem)System.Runtime.InteropServices.Marshal.GetObjectForIUnknown(rootCoordnateSystemPtr);
+                System.IntPtr rootCoordnateSystemPtr = UnityEngine.XR.WindowsMR.WindowsMREnvironment.OriginSpatialCoordinateSystem;
+                SpatialCoordinateSystem rootSpatialCoordinateSystem = (SpatialCoordinateSystem)System.Runtime.InteropServices.Marshal.GetObjectForIUnknown(rootCoordnateSystemPtr);
 
-                        // Get the relative transform from the unity origin
-                        System.Numerics.Matrix4x4? relativePose = CoordinateSystem.TryGetTransformTo(rootSpatialCoordinateSystem);
+                // Get the relative transform from the unity origin
+                System.Numerics.Matrix4x4? relativePose = CoordinateSystem.TryGetTransformTo(rootSpatialCoordinateSystem);
 
-                        if (relativePose != null)
-                        {
-                            System.Numerics.Vector3 scale;
-                            System.Numerics.Quaternion rotation1;
-                            System.Numerics.Vector3 translation1;
+                if (relativePose != null)
+                {
+                    System.Numerics.Vector3 scale;
+                    System.Numerics.Quaternion rotation1;
+                    System.Numerics.Vector3 translation1;
        
-                            System.Numerics.Matrix4x4 newMatrix = relativePose.Value;
+                    System.Numerics.Matrix4x4 newMatrix = relativePose.Value;
 
-                            // Platform coordinates are all right handed and unity uses left handed matrices. so we convert the matrix
-                            // from rhs-rhs to lhs-lhs 
-                            // Convert from right to left coordinate system
-                            newMatrix.M13 = -newMatrix.M13;
-                            newMatrix.M23 = -newMatrix.M23;
-                            newMatrix.M43 = -newMatrix.M43;
+                    // Platform coordinates are all right handed and unity uses left handed matrices. so we convert the matrix
+                    // from rhs-rhs to lhs-lhs 
+                    // Convert from right to left coordinate system
+                    newMatrix.M13 = -newMatrix.M13;
+                    newMatrix.M23 = -newMatrix.M23;
+                    newMatrix.M43 = -newMatrix.M43;
 
-                            newMatrix.M31 = -newMatrix.M31;
-                            newMatrix.M32 = -newMatrix.M32;
-                            newMatrix.M34 = -newMatrix.M34;
+                    newMatrix.M31 = -newMatrix.M31;
+                    newMatrix.M32 = -newMatrix.M32;
+                    newMatrix.M34 = -newMatrix.M34;
 
-                            System.Numerics.Matrix4x4.Decompose(newMatrix, out scale, out rotation1, out translation1);
-                            translation = new Vector3(translation1.X, translation1.Y, translation1.Z);
-                            rotation = new Quaternion(rotation1.X, rotation1.Y, rotation1.Z, rotation1.W);
-                            Pose pose = new Pose(translation, rotation);
-
-                            // If there is a parent to the camera that means we are using teleport and we should not apply the teleport
-                            // to these objects so apply the inverse
-                            if (CameraCache.Main.transform.parent != null)
-                            {
-                                pose = pose.GetTransformedBy(CameraCache.Main.transform.parent);
-                            }
-
-                            gameObject.transform.SetPositionAndRotation(pose.position, pose.rotation);
-                            //Debug.Log("Id= " + id + " QRPose = " +  pose.position.ToString("F7") + " QRRot = "  +  pose.rotation.ToString("F7"));
-                        }
-                        else
-                        {
-                           // Debug.Log("Id= " + id + " Unable to locate qrcode" );
-                        }
-                    }
-                    else
-                    {
-                       gameObject.SetActive(false);
-                    }
-#endif
+                    System.Numerics.Matrix4x4.Decompose(newMatrix, out scale, out rotation1, out translation1);
+                    translation = new Vector3(translation1.X, translation1.Y, translation1.Z);
+                    rotation = new Quaternion(rotation1.X, rotation1.Y, rotation1.Z, rotation1.W);
+                    pose = new Pose(translation, rotation);
+                    return true;
                 }
+                else
+                {
+                    // Debug.Log("Id= " + id + " Unable to locate qrcode" );
+                }
+#endif
+                return false;
             }
 
         }
